33d47e5798dd25f950064dddb3830fa33c0db674
Jordi Martinez <jmartinezg.fp@gmail.com>
2022-08-17 16:10:20 +0200

    6th project finished

--- 06-webserver/app.js
+++ 06-webserver/app.js
@@ -1,26 +1,44 @@
+require('dotenv').config();
 const express = require('express');
+const hbs = require('hbs');
+
 const app = express();
-const port = 8080;
+
+//Handlebars
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
 
 //Middlewares
 app.use(express.static('public')); //Serves static content
 
-// app.get('/hello-world', (req, res) => {
-//     res.send('Hello world');
-// });
+app.get('/', (req, res) => {
+    res.render('home', {
+        name: 'Jordi Martínez',
+        title: 'Node Course'
+    });
+})
+
 
 app.get('/generic', (req, res) => {
-    res.sendFile(__dirname + '/public/generic.html');
+    // res.sendFile(__dirname + '/public/generic.html');
+    res.render('generic', {
+        name: 'Jordi Martínez',
+        title: 'Node Course'
+    });
 })
 
 app.get('/elements', (req, res) => {
-    res.sendFile(__dirname + '/public/elements.html');
+    // res.sendFile(__dirname + '/public/elements.html');
+    res.render('elements', {
+        name: 'Jordi Martínez',
+        title: 'Node Course'
+    });
 })
 
 app.get('*', (req, res) => {
     res.sendFile(__dirname + '/public/404.html');
 });
 
-app.listen(port, () => {
-    console.log(`The server is running in the port ${port}`);
+app.listen(process.env.PORT, () => {
+    console.log(`The server is running in the port ${process.env.PORT}`);
 });
